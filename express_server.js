b6f2850e7e209473f95d6e3af4f7b1b4481b3cb2
Adrien Peynichou <adrien.peynichou@gmail.com>
2017-01-18 20:00:19 +0000

    Implemented the update button to modify URLs on the show page

--- express_server.js
+++ express_server.js
@@ -24,30 +24,22 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// app.get("/hello", (req, res) => {
-//   res.end("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  // console.log("urls/new");
   res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
-  console.log('params', req.params.id);
   let templateVars = {shortURL: req.params.id,
                       longURL: urlDatabase};
-  console.log(templateVars);
   res.render("urls_show", {templateVars: templateVars});
 });
 
-app.post("/urls", (req, res) => { // is that referring to /urls/create
-  // console.log(req.body.longURL);
+app.post("/urls", (req, res) => {
   let longURL = req.body.longURL;
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = longURL;
@@ -57,9 +49,7 @@ app.post("/urls", (req, res) => { // is that referring to /urls/create
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.id];
   var magicCode = req.params.shortURL;
-  console.log(req.params.shortURL);
   res.redirect(urlDatabase[magicCode]);
-  // res.redirect(longURL);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
@@ -70,10 +60,11 @@ app.post("/urls/:id/delete", (req, res) => {
 
 app.post("/urls/:id", (req, res) => {
   let key = req.params.id;
+  delete urlDatabase[key];
   let updatedURL = req.body.updatedURL;
   let updatedShort = generateRandomString();
-  delete urlDatabase[key];
   urlDatabase[updatedShort] = updatedURL;
+  // debugger;
   res.redirect('/urls');
 })
 
