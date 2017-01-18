635a2b2bad8248e30cca1e4b7bc5eeaac59e0a3f
Adrien Peynichou <adrien.peynichou@gmail.com>
2017-01-18 14:26:28 +0000

    Built index page, implemented a bootstrap header and got the server to redirect to the long URLs

--- express_server.js
+++ express_server.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+function generateRandomString() {
+  return Math.random().toString(36).slice(2,8);
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -10,26 +17,55 @@ var urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  res.redirect("urls");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.end("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  // console.log("urls/new");
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+  let templateVars = { shortURL: req.params.id };// , longURL: }; // find the o
+  if (urlDatabase.hasOwnProperty(shortURL)) {
+    res.redirect(urlDatabase.shortURL)
+  } else {
+  console.log("url/id");
   res.render("urls_show", templateVars);
-})
+  };
+});
+
+app.post("/urls", (req, res) => { // is that referring to /urls/create
+  // console.log(req.body.longURL);
+  let longURL = req.body.longURL;
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  // console.log(urlDatabase);
+  // res.send("Ok");
+  console.log(urlDatabase);
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.id];
+  var magicCode = req.params.shortURL;
+  console.log(req.params.shortURL);
+  res.redirect(urlDatabase[magicCode]);
+  // res.redirect(longURL);
+});
 
 app.listen(PORT, () => {
   console.log(`Tiny app listening on port ${PORT}!`);
